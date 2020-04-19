f3f7e03bb48557431dac769a3f8384e10732d541
AzzyC <azmath2000@gmail.com>
2020-04-19 02:36:14 +0100

    scripts: referencegen: Restrict User Input, more functions
    The script can finally generate a newspaper reference! I wanted to get this format completed first as it is most frequently used. Have to be careful of which variables to unset and which will be overridden

--- referencegen.sh
+++ referencegen.sh
@@ -1,23 +1,30 @@
-env -i bash # Clear any previous bash made variables
-echo -e "\nWelcome to YourReferenceGenerator (YRG)\n"
 
 author () {
-    read -p "How many authors are there? " noofauthor
+    unset noofauthor
+
+    while [[ ! $noofauthor =~ ^(1|2|3|4|5)$ ]]
+    do
+    echo -e "How many authors are there? - \e[3mMaximum of 5\e[0m:"
+    read noofauthor
+    if [[ ! $noofauthor =~ ^(1|2|3|4|5)$ ]]; then
+        echo -e "\nYou did not input a number or a number within the range: '$noofauthor'. Try again.\n"
+    fi
+    done
 
     if [[ "$noofauthor" -eq "1" ]]; then
-    echo -e "What is the authors fullname? (e.g. Forename Surname - \e[3mForename can be a letter\e[0m) "
+    echo -e "\nWhat is the authors fullname? (e.g. Forename Surname - \e[3mForename can be a letter\e[0m)"
     read author
     
     forename=$(echo "$author" | cut -d ' ' -f 1)
     surname=$(echo "$author" | cut -d ' ' -f 2)
 
-    authorcomplete=$(echo "${surname}, ${forename:0:1}. ")
+    authorcomplete=$(echo "${surname}, ${forename:0:1}.")
     echo "$authorcomplete" # This command is here for now to test
     fi
 
     if [[ "$noofauthor" -eq "2" ]]; then
-        echo -e "\nProvide both author names, using a comma to separate (e.g. Forename Surname, Forename Surname): "
-        read author
+        echo
+        read -p "Provide both author names, using a comma to separate (e.g. Forename Surname, Forename Surname): " author
         author=$(echo "$author" | sed 's/, /,/g') # Can only use one character as delimiter; comma is desired
 
         firstauthorfullname=$(echo "$author" | cut -d ',' -f 1)
@@ -36,7 +43,7 @@ author () {
     fi
 
     if [[ "$noofauthor" -ge "3" ]]; then
-        echo -e "\nList all $noofauthor author names, using a comma to separate (e.g. Forename Surname, Forename Surname, Forname Surname ... - \e[3mMaximum of 5\e[0m): "
+        echo -e "\nList all $noofauthor author names, using a comma to separate (e.g. Forename Surname, Forename Surname, Forname Surname ... : "
         read author
         author=$(echo "$author" | sed 's/, /,/g') # Can only use one character as delimiter; comma is desired
 
@@ -77,30 +84,59 @@ author () {
         fi
 
         if [[ "$noofauthor" -eq "3" ]]; then
-            authorcomplete=$(echo "$firstauthorcomplete, $secondauthorcomplete, $thirdauthorcomplete ")
+            authorcomplete=$(echo "$firstauthorcomplete, $secondauthorcomplete, $thirdauthorcomplete")
         fi
 
         if [[ "$noofauthor" -eq "4" ]]; then
-            authorcomplete=$(echo "$firstauthorcomplete, $secondauthorcomplete, $thirdauthorcomplete, $fourthauthorcomplete ")
+            authorcomplete=$(echo "$firstauthorcomplete, $secondauthorcomplete, $thirdauthorcomplete, $fourthauthorcomplete")
         fi
 
         if [[ "$noofauthor" -eq "5" ]]; then
-            authorcomplete=$(echo "$firstauthorcomplete, $secondauthorcomplete, $thirdauthorcomplete, $fourthauthorcomplete, $fifthauthorcomplete ")
+            authorcomplete=$(echo "$firstauthorcomplete, $secondauthorcomplete, $thirdauthorcomplete, $fourthauthorcomplete, $fifthauthorcomplete")
         fi
     fi
 
 }
 
+date=$(echo $(date '+%dth %B %Y'))
+dateaccessed=$(echo "[Accessed on $date]")
+
+namenewsmag () {
+    echo
+    read -p "What is the name of the Newspaper/Magazine? (e.g. The Guardian  or  Financial Times): " namenewsmag
+    namenewsmag=$(echo -e "\e[3m$namenewsmag\e[0m.")
+    echo "$namenewsmag" # This command is here for now to test
+}
+
 publishdate () {
+    echo
     read -p "What is the published date of the source? (e.g. 12th March, 2017): " pub
 
     pub=$(echo "$pub" | sed 's/, /,/g') # Can only use one character as delimiter; comma is desired
     pubyear=$(echo "$pub" | cut -d ',' -f 2 )
     pubdate=$(echo "$pub" | cut -d ',' -f 1 )
+
+    pubyear=$(echo "($pubyear)")
+    pubdate=$(echo "$pubdate".)
     echo "$pubyear" # This command is here for now to test
     echo "$pubdate" # This command is here for now to test
 }
 
+quotedtitle () {
+    echo
+    read -p "What is the title of the source? " quotedtitle
+    quotedtitle=$(echo "'$quotedtitle.'")
+    echo "$quotedtitle" # This command is here for now to test
+}
+
+url () {
+    echo
+    read -p "What is the URL of the source? " url
+}
+
+clear # Clear screen, immerse the generator
+
+echo -e "\nWelcome to YourReferenceGenerator (YRG)\n"
 echo -e "What format is the source you are referencing?"
 PS3='Please enter your number choice (1/2/3/4): '
 options=("(Online) Newspaper" "Website (General)" "Journal" "Quit") # Options to be slowly expanded
@@ -109,12 +145,16 @@ do
     case $opt in
         "(Online) Newspaper")
             echo -e "\nYou chose '$opt'"
-            echo -e "\nWe will now build to this style:"
-            echo "Author’s surname, Initial. (Year of publication) ‘Title of article.’ Name of newspaper (in"
-            echo -e "italics). [Online] Date of publication. [Date accessed] URL\n"
+            echo -e "\n"$opt" reference format:"
+            echo -e "Author’s surname, Initial. (Year of publication) ‘Title of article.’ \e[3mName of newspaper (in\e[0m"
+            echo -e "\e[3mitalics)\e[0m. [Online] Date of publication. [Date accessed] URL\n"
 
             author
+            namenewsmag
             publishdate
+            quotedtitle
+            url
+            echo -e "\nReference Generated:\n$authorcomplete $pubyear $quotedtitle $namenewsmag [Online] $pubdate $dateaccessed $url"
 	    break
             ;;
         "Website (General)")
